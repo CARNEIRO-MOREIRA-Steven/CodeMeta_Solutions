01a9cbb5c05fc3995a6bf6a7082a5b33f068a5eb
Squaad6z <carneiromoreirasteven22@gmail.com>
2024-08-29 17:47:43 +0200

    Modification page a propos css

--- src/components/APropos.jsx
+++ src/components/APropos.jsx
@@ -4,35 +4,38 @@ import './a_propos.css'
 const APropos = () => {
   return (
     <section className='a_propos_section'>
+        <section className='a_propos_de_moi'>
         <section className='a_propos_description'>
-        <h2 className='a_propos_title'>Qui suis-je ?</h2>
+        <h2 id='a_propos_title'>Qui suis-je ?</h2>
         <p>Je m'appelle CARNEIRO MOREIRA Steven, développeur web passionné et fondateur de CodeMeta Solutions. Avec une expertise en front-end et une approche centrée sur l'utilisateur, je m'efforce de transformer vos idées en sites web esthétiques, fonctionnels et performants.</p>
         </section>
         <section className='a_propos_description'>
-        <h2 className='a_propos_title'>Mon parcours</h2>
+        <h2 id='a_propos_title'>Mon parcours</h2>
         <p>Depuis mes débuts dans le développement web, j'ai toujours été fasciné par la manière dont le code peut donner vie à des idées. Après plusieurs années d'expérience dans diverses agences et entreprises, j'ai décidé de me lancer en tant que freelance pour offrir des solutions sur mesure et plus personnalisées à mes clients.</p>
         </section>
         <section className='a_propos_description'>
-            <h2 classname='a_propos_title'>Mon expertise</h2>
-            <p>e suis spécialisé dans le développement front-end, avec une expertise particulière en HTML, CSS, JavaScript, et des frameworks modernes comme Next.js et React. Mon objectif est de créer des interfaces utilisateur qui allient esthétique, fluidité, et performance.</p>
+        <h2 id='a_propos_title'>Mes réalisations</h2>
+        <p>J'ai eu le plaisir de collaborer avec diverses entreprises et startups pour créer des sites web et des applications qui répondent parfaitement à leurs besoins. Vous pouvez consulter quelques-unes de mes réalisations dans mon Portfolio.</p>
+        </section>
+        </section>
+        <section className='a_propos_expertise'>
+        <section className='a_propos_description'>
+            <h2 id='a_propos_title'>Mon expertise</h2>
+            <p>Je suis spécialisé dans le développement front-end, avec une expertise particulière en HTML, CSS, JavaScript, et des frameworks modernes comme Next.js et React. Mon objectif est de créer des interfaces utilisateur qui allient esthétique, fluidité, et performance.</p>
         </section>
         <section className='a_propos_description'>
-            <h2 classname='a_propos_title'>Mes valeurs</h2>
+            <h2 id='a_propos_title'>Mes valeurs</h2>
             <p>En tant que développeur indépendant, je m'engage à offrir des solutions de haute qualité, adaptées aux besoins spécifiques de chaque client. La transparence, la communication, et la satisfaction client sont au cœur de mon approche.</p>
         </section>
         <section className='a_propos_description'>
-            <h2 classname='a_propos_title'>Mon approche</h2>
+            <h2 id='a_propos_title'>Mon approche</h2>
             <p>Ma méthode de travail est simple et efficace : je commence par une discussion approfondie pour bien comprendre vos besoins et vos objectifs. Ensuite, je propose des solutions adaptées, tout en vous tenant informé à chaque étape du projet. Mon but est de rendre le processus de création aussi fluide que possible pour vous.</p>
         </section>
-        <section className='a_propos_description'>
-            <h2 classname='a_propos_title'>Mes réalisations</h2>
-            <p>J'ai eu le plaisir de collaborer avec diverses entreprises et startups pour créer des sites web et des applications qui répondent parfaitement à leurs besoins. Vous pouvez consulter quelques-unes de mes réalisations dans mon Portfolio.</p>
         </section>
-        <section className='a_propos_description'>
-            <h2 classname='a_propos_title'>Pourquoi me choisir ?</h2>
+        <section className='a_propos_description me_choisir'>
+            <h2 id='a_propos_title'>Pourquoi me choisir ?</h2>
             <p>En me choisissant, vous optez pour un partenaire unique qui s'investit pleinement dans votre projet. Mon expertise technique, combinée à une approche personnalisée, me permet de créer des solutions web qui ne sont pas seulement belles, mais aussi efficaces et adaptées à vos objectifs.</p>
         </section>
-
     </section>
   )
 }
