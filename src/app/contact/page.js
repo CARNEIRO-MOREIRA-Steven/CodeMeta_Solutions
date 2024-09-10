6c5be1991eccae875c64236c282455dcb68acc9e
Squaad6z <carneiromoreirasteven22@gmail.com>
2024-09-10 16:08:56 +0200

    Modification A propos + création Contact

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '@/components/Header'
+import ContactForm from '@/components/ContactForm'
 
 
 const page = () => {
   return (
     <div>
       <Header />
+      <ContactForm />
     </div>
   )
 }
