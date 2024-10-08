cdffb0e51fad5ff24b8e2136280ee2b4844ec888
Steven CARNEIRO MOREIRA <122810515+CARNEIRO-MOREIRA-Steven@users.noreply.github.com>
2024-10-08 12:09:20 +0200

    Update metadata.js Google

--- src/app/metadata.js
+++ src/app/metadata.js
@@ -5,6 +5,15 @@ const Metadata = ({ title, description, keywords }) => (
     <title>{title}</title>
     <meta name="description" content={description} />
     <meta name="keywords" content={keywords} />
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11453676363"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11453676363');
+</script>
   </head>
 );
 
